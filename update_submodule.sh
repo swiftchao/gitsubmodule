87942c98e867b77c8415c00d8bd7b31062867732
chaofei <chaofeibest@163.com>
2024-11-29 15:19:54 +0800

    【submodule】Add update submodule shell script

---
+++ update_submodule.sh
@@ -0,0 +1,8 @@
+#########################################################################
+# File Name: update_submodule.sh
+# Author: chaofei
+# mail: chaofeibest@163.com
+# Created Time: 2024-11-29 15:19:08
+#########################################################################
+#!/bin/bash
+git submodule update --init --recursive
